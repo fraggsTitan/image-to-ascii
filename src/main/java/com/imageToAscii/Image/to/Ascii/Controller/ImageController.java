bec5ed2bd298ad3ec9a004b86312be990ed0f97c
fraggsTitan <vishnu.sara.kuma@gmail.com>
2025-11-22 20:40:51 +0530

    Minor Changes made to ImageController to make it more structured

--- src/main/java/com/imageToAscii/Image/to/Ascii/Controller/ImageController.java
+++ src/main/java/com/imageToAscii/Image/to/Ascii/Controller/ImageController.java
@@ -9,35 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.IllegalFormatException;
 import java.util.List;
 
 @RestController
 public class ImageController {
     private static final ASCIIWriter asciiWriter=new ASCIIWriter();
     @PostMapping("/ascii")
-    public List<CharCountMap> convertImage(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) int height,
-                                           @RequestParam(required = false) int width,@RequestParam(required = false) int scale,@RequestParam String format) throws IOException {
+    public List<CharCountMap> convertImage(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) Integer height,
+                                           @RequestParam(required = false) Integer width,@RequestParam(required = false) Integer scale,@RequestParam String format) throws IOException {
        if(format.equalsIgnoreCase("GIF"))throw new UnexpectedFormatException("Received GIF Format image in method that doesn't handle it. Try /ascii/gif instead");
        BufferedImage img= ImageIO.read(file.getInputStream());
        return asciiWriter.buildImage(img, ColorMap.valueOf(mapping.toUpperCase()),width,height,scale,format);
     }
     @PostMapping("/ascii/gif")
-    public List<GIFCharMap>convertGIF(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) int height,
-                                      @RequestParam(required = false) int width,@RequestParam(required = false) int scale,@RequestParam String format) throws IOException {
-        if(!format.equalsIgnoreCase("GIF"))throw new UnexpectedFormatException("Received non-GIF Format while GIF was expected. Try /ascii instead");
+    public List<GIFCharMap>convertGIF(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) Integer height,
+                                      @RequestParam(required = false) Integer width,@RequestParam(required = false) Integer scale) throws IOException {
         ImageInputStream stream=ImageIO.createImageInputStream(file.getInputStream());
         return asciiWriter.buildGif(stream, ColorMap.valueOf(mapping.toUpperCase()),width,height,scale);
     }
     @PostMapping("/ascii/crop_and_build")
-    public List<CharCountMap>cropAndBuildImage(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) int height,
-                                             @RequestParam(required = false) int width,
-                                             @RequestParam(required = false) int scale,@RequestParam String format,
+    public List<CharCountMap>cropAndBuildImage(@RequestParam MultipartFile file, @RequestParam String mapping, @RequestParam(required = false) Integer height,
+                                             @RequestParam(required = false) Integer width,
+                                             @RequestParam(required = false) Integer scale,@RequestParam String format,
                                              @RequestParam int x, @RequestParam int y,@RequestParam int cropWidth,@RequestParam int cropHeight) throws IOException {
         if(format.equalsIgnoreCase("GIF")){
             throw new UnexpectedFormatException("Received GIF Format image in method that doesn't handle it. Try /ascii/gif instead. If you wanted to crop a GIF, it isnt supported yet.");
